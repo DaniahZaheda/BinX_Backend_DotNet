3994d8d72b5697a7124bf6eca42c586966f652ba
DaniahZaheda <daniahzaheda@gmail.com>
2026-08-09 22:50:24 +0300

    Week4 day1

---
+++ Week4/Day1/IdentityDemo/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+using IdentityDemo.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterDto request)
+    {
+        var user = new IdentityUser
+        {
+            UserName = request.Email,
+            Email = request.Email
+        };
+
+        var result = await _userManager.CreateAsync(
+            user,
+            request.Password
+        );
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok(new
+        {
+            message = "User registered successfully"
+        });
+    }
+}
